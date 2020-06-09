2c7ddff6696e165178c4a37201b03a3386fe5f3c
Jsquad <lakis006@umn.edu>
2020-06-09 13:55:37 -0500

    added content to the manager.js

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+
+getRole() {
+    return "Manager";
+}
+
+getOfficeNumber() {
+    return this.officeNumber;
+}
+
+}
+
+const manager = new Manager();
+
+module.exports = Manager;
